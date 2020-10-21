194a6d88d7e2226ac82be1a6e418c78befddeb1b
mittalprince <princemittal766@gmail.com>
2020-10-21 17:52:37 +0530

    Add Server Code

--- app.js
+++ app.js
@@ -1,41 +1,97 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const dotenv = require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var express = require("express")
+var path = require("path")
+var favicon = require("serve-favicon")
+var logger = require("morgan")
+var cookieParser = require("cookie-parser")
+var bodyParser = require("body-parser")
+var expressHbs = require("express-handlebars")
+var mongoose = require("mongoose")
+var session = require("express-session")
+var passport = require("passport")
+var flash = require("connect-flash")
+var validator = require("express-validator")
+var MongoStore = require("connect-mongo")(session)
 
-var app = express();
+var routes = require("./routes/index")
+var userRoutes = require("./routes/users")
+
+var app = express()
+
+const mongo_uri = process.env.mongo_uri;
+const connect = mongoose.connect(mongo_uri, { useUnifiedTopology: true, useNewUrlParser: true});
+connect.then((db)=>{
+    console.log("Database Connected Successfully");
+}, (err)=>{
+    console.log("Error occur while connecting ", err);
+})
+
+require("./config/passport")
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }))
+app.set("view engine", ".hbs")
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")))
+app.use(logger("dev"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(validator())
+app.use(cookieParser())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use(
+  session({
+    secret: "mysupersecret",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 180 * 60 * 1000 }
+  })
+)
+
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.static(path.join(__dirname, "public")))
+
+app.use(function(req, res, next) {
+  res.locals.login = req.isAuthenticated()
+  res.locals.session = req.session
+  next()
+})
+
+app.use("/user", userRoutes)
+app.use("/", routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  var err = new Error("Not Found")
+  err.status = 404
+  next(err)
+})
 
-// error handler
+// error handlers -------------------
+
+// development error handler
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    res.render("error", {
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+// production error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500)
+  res.render("error", {
+    message: err.message,
+    error: {}
+  })
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = app
